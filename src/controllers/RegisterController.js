5a374f49156548037147798e4a794460a4c7b8c1
jcagz96 <jcagz@iscte-iul.pt>
2019-11-01 16:17:07 +0000

    new login using fortnite-basic-api

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,68 +1,88 @@
 const Profile = require('../models/Profile');
 const bcrypt = require('bcryptjs');
 const Fortnite = require("fortnite-api");
- 
+
+const { Client, Communicator, FriendStatus } = require('fortnite-basic-api');
+
 const axios = require('axios')
 
 module.exports = {
-    async store(req,res){
-        const { key, location: profilePicUrl = ""} = req.file;
+    async store(req, res) {
+        const { key, location: profilePicUrl = "" } = req.file;
         const { name, fortniteUsername, password, email, plataform, file } = req.body;
-        
-       
+
+
 
         console.log(`[ RegisterController.js ]  -  ${name},  ${email}  ,  ${fortniteUsername} , ${password}  ,  ${profilePicUrl} , ${plataform}`);
 
         var errors = {};
 
-        const userExists = await Profile.findOne({$or: [
-            {email: email},
-            {fortniteUsername: fortniteUsername}
-        ]})
+        const userExists = await Profile.findOne({
+            $or: [
+                { email: email },
+                { fortniteUsername: fortniteUsername }
+            ]
+        })
 
-        if (userExists){
+        if (userExists) {
             errors['errorUsername'] = "username or email already registered";
             return res.json(errors)
         }
 
-        
+        const client = new Client({
+            email: email,
+            password: password,
+            launcherToken: process.env.CLIENT_LAUNCHER_TOKEN,
+            fortniteToken: process.env.FORTNITE_CLIENT_TOKEN,
+        });
 
-        let fortniteAPI = new Fortnite(
-            [
-                email,
-                password,
-                process.env.CLIENT_LAUNCHER_TOKEN,
-                process.env.FORTNITE_CLIENT_TOKEN,
-            ]
-        );
 
-         await fortniteAPI.login().then(() => {
-            console.log(`[ RegisterController.js ]  -  Login Bem sucedido, logo a conta existe`)
-        }).catch((error)=>{
-            console.log(`[ RegisterController.js ]  -  Login Falhou, logo esta ou nao existe ou tem as credenciais erradas`)
-            errors['errorLoginFailed'] = "login failed | cause: bad credentials maybe";
+        await client.login().then(logino => {
+            if (logino.success) {
+                console.log(`[ RegisterController.js ]  -  Login Bem sucedido, logo a conta existe`)
+            }
+            if (logino.error) {
+                console.log(logino.error.message)
+                console.log(`[ RegisterController.js ]  -  Login Falhou, logo esta ou nao existe ou tem as credenciais erradas`)
+                errors['errorLoginFailed'] = "login failed | cause: bad credentials maybe";
+            }
         })
+
+        /*
+        (async () => {
+
+            const login = await client.login();
+
+            if (login.success) {
+                console.log(`[ RegisterController.js ]  -  Login Bem sucedido, logo a conta existe`)
+            }
+            if (login.error) {
+                console.log(login.error.message)
+                console.log(`[ RegisterController.js ]  -  Login Falhou, logo esta ou nao existe ou tem as credenciais erradas`)
+                errors['errorLoginFailed'] = "login failed | cause: bad credentials maybe";
+            }
+        })(); */
 
         const url = `${process.env.APP_URL}/finduser`
 
-        var response =  await axios.post(url, {
+        var response = await axios.post(url, {
             username: fortniteUsername,
             plataform: plataform,
         })
 
 
-        if(response.data.error === "Player Not Found"){
+        if (response.data.error === "Player Not Found") {
             errors['errorUsername'] = "username not found";
         }
-        else{
+        else {
             console.log(`[ RegisterController.js ]  -  o username: ${fortniteUsername} existe`)
         }
 
-        if(Object.keys(errors).length > 0){
+        if (Object.keys(errors).length > 0) {
 
             return res.json(errors)
         }
-        else{
+        else {
             console.log(`[ RegisterController.js ]  -   0 errors`)
 
             const salt = await bcrypt.genSalt(10);
@@ -74,7 +94,7 @@ module.exports = {
                 name,
                 fortniteUsername,
                 email,
-                password : hashedpassword,
+                password: hashedpassword,
                 plataform,
                 profilePicUrl,
                 key,
